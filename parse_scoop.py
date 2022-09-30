6c9dae30abc1659f438fb17cb8393a16e1879ab4
Lorenzo Velasco Sánchez <lorenzo.velasco.sanchez@pinch.nl>
2022-09-30 10:30:46 +0200

    Some error handling in parse_scoop.py

--- parse_scoop.py
+++ parse_scoop.py
@@ -1,5 +1,6 @@
 import os
 from enum import Enum
+import logging, sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -30,10 +31,14 @@ def parseHtml(url: str):
     Returns:
         ScoopData: All the parsed data from the html
     """    
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
 
-    return ScoopData(soup)
+        return ScoopData(soup)
+    except:
+        logging.debug("Could not parse soup from html.")
+        raise
     
 
 
@@ -46,18 +51,27 @@ def downloadAppLogo(soup: BeautifulSoup, output_path: str = "img/app_icon.png"):
     Returns:
         str: Path with file
     """
-    appIcon = soup.find("img", {"class": "app-icon"})
-    appIconUrl = appIcon['src']
+    # Make dirs for the app logo
+    try:
+        full_output_path = f"img/{output_path}"
+        path_with_file = f"{full_output_path}/app_icon.png"
+        path_exist = os.path.exists(full_output_path)
+        if not path_exist:
+            os.makedirs(full_output_path)
+    except:
+        path_with_file = f"img/app_icon.png"
 
-    full_output_path = f"img/{output_path}"
-    path_with_file = f"{full_output_path}/app_icon.png"
-    path_exist = os.path.exists(full_output_path)
-    if not path_exist:
-        os.makedirs(full_output_path)
-
-    with open(path_with_file, "wb") as f:
-        f.write(requests.get(appIconUrl).content)
-        print(f"Saved app_icon to {full_output_path}/app_icon.png")
+    # Download image from scoop and save to dir
+    try:
+        appIcon = soup.find("img", {"class": "app-icon"})
+        appIconUrl = appIcon['src']
+        with open(path_with_file, "wb") as f:
+            f.write(requests.get(appIconUrl).content)
+            print(f"Saved app_icon to {full_output_path}/app_icon.png")
+    except:
+        logging.debug("Could not save app icon to file, maybe it's non-existant?")
+        logging.debug("Using default app_icon.png")
+        path_with_file = f"img/default_app_icon.png"
     
     return path_with_file
 
@@ -72,8 +86,13 @@ def getUrlFromButtonWithId(text: str, soup: BeautifulSoup):
         Url of the button requested
     """    
     button_div = soup.select_one(f'div[id*="{text}-button"]')
-    # print(button_div.get_text().strip())
-    return button_div.find("a").get("href")
+    button_url = button_div.find("a").get("href")
+
+    if (button_url is None):
+        logging.debug(f"Could not find the button url with id: {text}-button. Returning ''")
+        return ""
+    else:
+        return button_url
 
 
 def getAppTitle(soup: BeautifulSoup) -> str:
@@ -86,8 +105,12 @@ def getAppTitle(soup: BeautifulSoup) -> str:
         str: App title
     """    
     title_div = soup.select_one(selector=f'div[class*="app-title"]')
-    app_title = title_div.get_text().strip()
-    return app_title
+
+    if (title_div is None):
+        logging.debug(f"Could not find the title div with class: app-title. Returning ''")
+        return ""
+    else:
+        return title_div.get_text().strip()
 
 
 def getPlatform(soup: BeautifulSoup) -> AppPlatform:
@@ -104,4 +127,6 @@ def getPlatform(soup: BeautifulSoup) -> AppPlatform:
 
 
 if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    # Demo URL
     parseHtml(url="https://scoop.pinch.nl/?page=app-detail&hash=e09e1ddb325632953fbc107d3de7741d&version=114")
