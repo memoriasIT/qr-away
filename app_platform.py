6d29b3bb557a95290330102f3804630ffdf8a2b6
Lorenzo Velasco Sánchez <lorenzo.velasco.sanchez@pinch.nl>
2022-09-30 13:11:51 +0200

    Finish generate card and some refactoring of structure of files

---
+++ app_platform.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class AppPlatform(Enum):
+    ANDROID = 1
+    IOS = 2
+    UNKNOWN = 3
--- generate_card.py
+++ generate_card.py
@@ -1,5 +1,5 @@
-from asyncore import write
 from PIL import Image, ImageDraw, ImageFont
+from app_platform import AppPlatform
 
 def pasteOnTop(im1, im2):
     w = max(im1.size[0], im2.size[0])
@@ -12,7 +12,7 @@ def pasteOnTop(im1, im2):
     return im
 
 def writeCenteredTitle(text: str):
-    font_path = 'font/RobotoCondensed-Bold.ttf'
+    font_path = 'res/font/RobotoCondensed-Bold.ttf'
     selected_size = 1
     for size in range(1, 150):
         font = ImageFont.FreeTypeFont(font_path, size=size)
@@ -30,7 +30,7 @@ def writeCenteredTitle(text: str):
     draw.text((final_img.width//2, 650), text, fill='black', anchor='mm', font=font)
 
 def writeCenteredSubtitle(text: str):
-    font_path = 'font/Roboto-Light.ttf'
+    font_path = 'res/font/Roboto-Light.ttf'
     selected_size = 1
     for size in range(1, 150):
         font = ImageFont.FreeTypeFont(font_path, size=size)
@@ -45,11 +45,21 @@ def writeCenteredSubtitle(text: str):
 
     font = ImageFont.FreeTypeFont(font_path, size=selected_size)
     draw = ImageDraw.Draw(final_img)
-    draw.text((final_img.width//2, 720), text, fill='black', anchor='mm', font=font)
+    draw.text((final_img.width//2, 710), text, fill='black', anchor='mm', font=font)
+
+def pastePlatformIcon(platform: AppPlatform):
+    if platform is AppPlatform.ANDROID:
+        platform_icon = Image.open("res/android-icon.png")
+    elif platform is AppPlatform.IOS:
+        platform_icon = Image.open("res/ios-icon.png")
+
+    platform_icon = platform_icon.resize((57, 57))
+    final_img.paste(platform_icon, (final_img.width//2-30, 750), platform_icon)
+
 
 
 img = Image.open("out/qr.png")
-card_mask = Image.open("card_mask.png")
+card_mask = Image.open("res/card_mask.png")
 
 # Paste qr on top and mask out to have rounded corners
 final_img = pasteOnTop(card_mask, img)
@@ -59,9 +69,7 @@ text = "VERY T"
 version = "v2.0.3"
 writeCenteredTitle(text)
 writeCenteredSubtitle(version)
-
-# draw text in center of rectangle 200x100        
-
+pastePlatformIcon(AppPlatform.IOS)
 
 
 final_img.show("test.png")
